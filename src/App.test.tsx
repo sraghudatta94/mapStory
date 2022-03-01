6a48a25aaf5f91e8350948e4b291c9f122eb34be
sraghudatta94 <sraghudatta94@gmail.com>
2022-03-01 11:27:26 +0530

    adding normal test case for checking if user email address is exist

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,16 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
+// polly-fill for matchMedia
+global.matchMedia = global.matchMedia || function () {
+  return {
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+  };
+};
+
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const userEmail = screen.getByText(/user@email.com/i);
+  expect(userEmail).toBeInTheDocument();
 });
